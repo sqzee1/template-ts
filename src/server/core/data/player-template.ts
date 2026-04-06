9b6cb0e2a5b3fdc5d8c20b5e97c66dfc9fab93ed
sqzeee1 <sqzgithub@gmail.com>
2026-04-06 00:11:56 -0300

    upd

---
+++ src/server/core/data/player-template.ts
@@ -0,0 +1,3 @@
+export interface PlayerTemplate {
+	Coins: number;
+}
---
+++ src/server/core/services/player-data-service.ts
@@ -0,0 +1,54 @@
+import { Service } from "@flamework/core";
+import ProfileStore from "@rbxts/profile-store";
+import { Players } from "@rbxts/services";
+import { PlayerTemplate } from "../data/player-template";
+
+type Profile = ProfileStore.Profile<PlayerTemplate>;
+@Service({})
+export class PlayerDataService {
+	private Template: PlayerTemplate = {
+		Coins: 0,
+	};
+
+	private Profiles = new Map<Player, Profile>();
+	private PlayerDataStore = ProfileStore.New("PlayerDataStore", this.Template);
+
+	private initializePlayerData(player: Player, profile: Profile) {}
+
+	private getProfile(player: Player): Profile | undefined {
+		return this.Profiles.get(player);
+	}
+
+	public init(player: Player): void {
+		const profile = this.PlayerDataStore.StartSessionAsync(`Player_${player.UserId}`, {
+			Cancel: () => {
+				return player.Parent !== Players;
+			},
+		});
+
+		if (!profile) {
+			player.Kick("Failed to load data");
+			return;
+		}
+
+		profile.OnSessionEnd.Connect(() => {
+			this.Profiles.delete(player);
+			player.Kick("Session end");
+		});
+
+		if (player.Parent === Players) {
+			this.Profiles.set(player, profile);
+			this.initializePlayerData(player, profile);
+		} else {
+			profile.EndSession();
+		}
+	}
+
+	public remove(player: Player): void {
+		const profile = this.Profiles.get(player);
+		if (!profile) return;
+
+		profile.EndSession();
+		this.Profiles.delete(player);
+	}
+}
