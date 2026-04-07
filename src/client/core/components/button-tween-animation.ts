00da16ab7f92eca9a9149d1aa1651ac40a2a2a28
sqzeee1 <sqzgithub@gmail.com>
2026-04-07 20:08:05 -0300

    upd

---
+++ src/client/core/components/button-tween-animation.ts
@@ -0,0 +1,17 @@
+import type { OnStart } from "@flamework/core";
+
+import BaseButtonAnimation from "./base-button-animation";
+
+export default abstract class ButtonTweenAnimation<A extends {} = {}, I extends GuiButton = GuiButton>
+	extends BaseButtonAnimation<A, I>
+	implements OnStart
+{
+	protected abstract readonly tweenInfo: TweenInfo;
+
+	protected abstract active?(): void;
+	protected abstract inactive?(): void;
+
+	public onStart(): void {
+		super.onStart();
+	}
+}
