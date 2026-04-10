b285b2fd14563641547c5095a6a30849073b7e5d
sqzeee1 <sqzgithub@gmail.com>
2026-04-10 17:19:30 -0300

    upd

---
+++ src/server/core/services/cooldown-service.ts
@@ -0,0 +1,21 @@
+import { Service } from "@flamework/core";
+
+@Service({})
+export class CooldownService {
+	private cooldowns = new Map<string, number>();
+
+	private getKey(player: Player, action: string): string {
+		return `${player.UserId}:${action}`;
+	}
+
+	public onCooldown(player: Player, action: CooldownActionsType, duration: number): boolean {
+		const key = this.getKey(player, action);
+		const now = os.clock();
+		const lastUsed = this.cooldowns.get(key) ?? 0;
+
+		if (now - lastUsed <= duration) return true;
+
+		this.cooldowns.set(key, now);
+		return false;
+	}
+}
