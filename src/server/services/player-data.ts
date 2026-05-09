a95771618e35dc33e29cfefe22ee81ba6c6d81a5
sqzee1 <sqzgithub@gmail.com>
2026-05-09 03:12:32 -0300

    upd

--- src/server/services/player-data.ts
+++ src/server/services/player-data.ts
@@ -1,11 +1,12 @@
-import { Service } from "@flamework/core";
+import { OnStart, Service } from "@flamework/core";
 import ProfileStore from "@rbxts/profile-store";
 import { Players } from "@rbxts/services";
+import { OnPlayerJoin, OnPlayerLeave } from "server/hook-managers/hooks";
 
 type Profile = ProfileStore.Profile<PlayerTemplate>;
 
 @Service({})
-export class PlayerDataService {
+export class PlayerDataService implements OnPlayerJoin, OnPlayerLeave, OnStart {
   private Template: PlayerTemplate = {
     Coins: 0,
   };
@@ -15,11 +16,11 @@ export class PlayerDataService {
 
   private initializePlayerData(player: Player, profile: Profile) {}
 
-  private getProfile(player: Player): Profile | undefined {
+  public getProfile(player: Player): Profile | undefined {
     return this.Profiles.get(player);
   }
 
-  public init(player: Player): void {
+  private initProfile(player: Player): void {
     const profile = this.PlayerDataStore.StartSessionAsync(`Player_${player.UserId}`, {
       Cancel: () => {
         return player.Parent !== Players;
@@ -44,11 +45,29 @@ export class PlayerDataService {
     }
   }
 
-  public remove(player: Player): void {
+  private removeProfile(player: Player): void {
     const profile = this.Profiles.get(player);
     if (!profile) return;
 
     profile.EndSession();
     this.Profiles.delete(player);
+  }
+
+  onPlayerJoin(player: Player): void {
+    this.initProfile(player);
+  }
+
+  onPlayerLeave(player: Player): void {
+    this.removeProfile(player);
+  }
+
+  onStart(): void {
+    game.BindToClose(() => {
+      for (const player of Players.GetPlayers()) {
+        task.spawn(() => {
+          this.removeProfile(player);
+        });
+      }
+    });
   }
 }
