efd996fb8f872f168765b8d8303851323f9359b8
sqzee1 <sqzgithub@gmail.com>
2026-09-11 22:05:09 -0300

    feat: update types

--- types/game.d.ts
+++ types/game.d.ts
@@ -1,3 +1,5 @@
+// biome-ignore-all lint: Types are consumed externally
+
 interface CharacterModel extends Model {
   Humanoid: Humanoid;
   HumanoidRootPart: Part;
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,5 @@
+// biome-ignore-all lint: Types are consumed externally
+
 type Maybe<T> = T | undefined;
 
 type NumberKeys<T> = {
